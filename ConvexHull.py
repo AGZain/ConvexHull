973a1ee2e6d20a76b529cc8a2253137d904c8098
abdulghaffar <abdulzainulabedin@gmail.com>
2018-03-31 19:52:44 -0400

    All functionality should be wokring

--- ConvexHull.py
+++ ConvexHull.py
@@ -1,84 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
-x = [3,4,6,4,2,3,5]
-y = [2,1,3,7,4,5,4]
-
-left = (0,float(x[0]),float(y[0]))
-right = (1,float(x[1]),float(y[1]))
-
-for i in range(0,len(x)):
-    x[i] = float(x[i])
-    y[i] = float(y[i])
-    if x[i] < left[1]:
-        left = (i, x[i], y[i])
-    if x[i] > right[1]:
-        right = (i, x[i], y[i])
-
-print(left)
-print(right)
+x = []
+y = []
+left = []
+right = []
 Upper = []
 Lower = []
+line = []
+lineBottom = []
+
+with open('data/example2.csv', 'r', encoding='utf8',errors='ignore') as csvfile:		#Opening up dictionary
+    read = csv.reader(csvfile, delimiter=',', quotechar='|')
+    count = 0
+    for row in read:
+        if count == 0:
+            count += 1
+            continue
+        count += 1
+        x.append(row[0])
+        y.append(row[1])
+# x = [2,3,3,4,4,5,3.5,2,5]
+# y = [2,3,4,3,4,2,5,3,3]
 
 for i in range(0,len(x)):
-    if i == left[0]:
-        continue
-    if y[i] > left[2] or y[i] > right[2]:
-        Upper.append(i)
-    if y[i] < left[2] or y[i] < right[2]:
-        Lower.append(i)
-print(Upper)
-line = []
-line.append(left[0])
-Upper.append(right[0])
-Lower.append(left[0])
-print("Upper:")
-print(Upper)
-Current = left[0]
-#for the Upper half
-for z in range(0,len(Upper)):
+    x.append(x[i])
+    y.append(y[i])
 
-    savedSlope = (-999.,0)
-    print("New Loop! " + str(len(Upper)))
+
+    left= (0,float(x[0]),float(y[0]))
+    right = (1,float(x[1]),float(y[1]))
+
+    for i in range(0,len(x)):
+        x[i] = float(x[i])
+        y[i] = float(y[i])
+        if x[i] <= left[1]:
+            if x[i] == left[1] and y[i] < left[2]:
+                left = (i, x[i], y[i])
+            if x[i] < left[1]:
+                left = (i, x[i], y[i])
+        if x[i] >= right[1]:
+            if x[i] == right[1] and y[i] > right[2]:
+                right = (i, x[i], y[i])
+            if x[i] > right[1]:
+                right = (i, x[i], y[i])
+
+    for i in range(0,len(x)):
+        if y[i] > left[2] or y[i] > right[2]:
+            Upper.append(i)
+        else:
+            Lower.append(i)
+    Upper.append(right[0])
+    Lower.append(left[0])
+    line.append(left[0])
+    lineBottom.append(right[0])
+
+
+Current = left[0]
+for z in range(0,len(Upper)):
+    savedSlope = (-999.,-1)
     for i in Upper:
-        print(i)
-        print("Looking at: " + str(x[i]) + "   " + str(y[i]))
         if x[i] > x[Current]:
             slope = (y[i]-y[Current])/(x[i] - x[Current])
-            print(slope)
             if savedSlope[0] < slope:
                 savedSlope = (slope,i)
-                Current = i
-                print(str(x[i]) + "GOOD " + str(y[i]))
-            #Need to look at other side of slope too. Will add later
+        elif(x[i] == x[Current] and y[i] > x[Current]):
+            savedSlope = (999999999999999999,i)
     line.append(savedSlope[1])
 
-    if Current == right[0]:
+    if savedSlope[1] != -1:
+        Current = savedSlope[1]
+        Upper.remove(savedSlope[1])
+    if Current == right[0] or len(Lower) == 0:
         break
 
-    Upper.remove(savedSlope[1])
-lineBottom = []
-lineBottom.append(right[0])
-print(line)
 Current = right[0]
-#For the bottom half:
-print("LOWER ISsssssssssssssssssssssssssssssssssssssssssss")
-print(Lower)
+
 for z in range(0,len(Lower)):
 
     savedSlope = (-99999.,-1)
-    print("New Loop! Lower:  " + str(len(Lower)))
     for i in Lower:
-        print(i)
-        print("Looking at: " + str(x[i]) + "   " + str(y[i]))
         if x[i] < x[Current]:
             slope = (y[i]-y[Current])/(x[i] - x[Current])
-            print("SLOPE IS..")
-            print(slope)
             if savedSlope[0] < slope:
                 savedSlope = (slope,i)
-                print(str(x[i]) + "GOOD " + str(y[i]))
-            #Need to look at other side of slope too. Will add later
+        elif(x[i] == x[Current] and y[i] < x[Current]):
+            savedSlope = (999999999999999999,i)
     lineBottom.append(savedSlope[1])
 
 
@@ -87,8 +95,8 @@ for z in range(0,len(Lower)):
         Lower.remove(savedSlope[1])
     if Current == left[0] or len(Lower) == 0:
         break
-print(lineBottom)
-plt.scatter(x,y,marker='o')
+
+plt.scatter(x,y,c='b',marker='o')
 i = 0
 while i < len(line)-1:
     plt.plot([x[line[i]],x[line[i+1]]],[y[line[i]],y[line[i+1]]],'r--')
